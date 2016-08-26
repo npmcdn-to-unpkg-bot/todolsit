ac10358bc0bd23594a639f50accecf667263969a
Nicolai Tellefsen <niccofyren@gmail.com>
2016-08-26 19:21:12 +0200

    Functional php API

--- api/php/index.php
+++ api/php/index.php
@@ -1,8 +1,11 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
 $method = $_SERVER['REQUEST_METHOD'];
 $requestUri = $_SERVER['REQUEST_URI'];
 $resource = '';
 $id = '';
+$requestData = json_decode(file_get_contents('php://input'));
 
 // Try to detect what resource (and id) we are working with
 $path = explode("/", $requestUri);
@@ -25,16 +28,16 @@ switch ($resource) {
 // Call methods based on request type
 switch ($method) {
 	case 'GET':
-		$apiHandler->get();
+		$apiHandler->get($id);
 		break;
 	case 'POST':
-		$apiHandler->post();
+		$apiHandler->post($requestData);
 		break;
 	case 'PUT':
-		$apiHandler->put();
+		$apiHandler->put($id, $requestData);
 		break;
 	case 'DELETE':
-		$apiHandler->delete();
+		$apiHandler->delete($id);
 		break;
 	default:
 		die();
@@ -43,17 +46,73 @@ switch ($method) {
 
 // Basic task api class
 class taskApi {
-	public function get() {
-		echo "get";
+	private $conn; 
+
+	function __construct() {
+		$config = new \Doctrine\DBAL\Configuration();
+		$connectionParams = array(
+			'path' => '../../db/todo.sqlite',
+			'user' => '',
+			'password' => '',
+			'host' => 'localhost',
+			'driver' => 'pdo_sqlite',
+		);
+		$this->conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
 	}
-	public function post() {
-		echo "post";
+
+	public function get($id = '') {
+		if($id == '') {
+			$tasks = $this->conn->fetchAll('SELECT * FROM task');
+		} else {
+			$tasks = Array($this->conn->fetchAssoc('SELECT * from task WHERE id = ?', array($id)));
+		}
+
+		if(count($tasks) > 0 && $tasks[0] != false) {
+			echo json_encode($tasks);
+			http_response_code(200);
+		} else {
+			http_response_code(404);
+		}
 	}
-	public function put() {
-		echo "put";
+
+	public function post($requestData) {
+		$result = $this->conn->insert('task', array(
+			'title' => $requestData->title,
+			'description' => $requestData->description,
+			'status' => $requestData->status ? 1 : 0,
+		));
+
+		if($result != false) {
+			$id = $this->conn->lastInsertId();
+			echo json_encode($this->conn->fetchAssoc('SELECT * FROM task WHERE id = ?', array($id)));
+			http_response_code(201);
+		} else {
+			http_response_code(500);
+		}
 	}
-	public function delete() {
-		echo "delete";
+
+	public function put($id, $requestData) {
+		$success = $this->conn->update('task', array(
+			'title' => $requestData->title,
+			'description' => $requestData->description,
+			'status' => $requestData->status ? 1 : 0,
+		), array('id' => $id));
+
+		if($success) {
+			http_response_code(200);
+		} else {
+			http_response_code(304);
+		}
+	}
+
+	public function delete($id) {
+		$success = $this->conn->delete('task', array('id' => $id));
+
+		if($success) {
+			http_response_code(200);
+		} else {
+			http_response_code(404);
+		}
 	}
 }
 ?>
